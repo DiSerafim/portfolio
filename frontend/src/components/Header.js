fbd924964431b0272cd11d0582a495d68b44c1b9
DiSerafim <diegoserafim1@gmail.com>
2024-12-24 12:10:07 -0300

    limpando a branch master

--- frontend/src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header = () => {
-    return <header>Menu Portifolio</header>;
-};
-
-export default Header;
--- tabela_contacts.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE portfolio_db.contacts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    message TEXT,
-    date_received DATE
-);
