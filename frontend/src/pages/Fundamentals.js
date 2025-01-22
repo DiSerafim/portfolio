e66a3f971ae8d0416c428bae1ef08c35ac97d767
DiSerafim <diegoserafim1@gmail.com>
2025-01-22 19:53:58 -0300

    criado as funções para editar e apagar uma postagem no frontend

--- frontend/src/pages/Fundamentals.js
+++ frontend/src/pages/Fundamentals.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Fundamentals.css";
+import { FaEdit } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
 
 const Fundamentals = () => {
     const [posts, setPosts] = useState([]);
     const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
     const [loading, setLoading] = useState(true);
+    const [editPostId, setEditPostId] = useState(null); // Abre e fecha formulário
+    const [formData, setFormData] = useState({ title: "", content: "", images: "", links: "", codes: "" });
 
     // Busca os dados na API
     const fetchPosts = async (pageNumber = 1) => {
@@ -33,7 +37,6 @@ const Fundamentals = () => {
         return <p className="loading">Carregando...</p>;
     }
     
-
     // Paginação
     const handlePageChange = (direction) => {
         const newPage = direction === "next" ? page + 1 : page - 1;
@@ -42,6 +45,57 @@ const Fundamentals = () => {
             fetchPosts(newPage);
         }
     }
+
+    // Editar postagem (Abre o Formulário)
+    const handleEdit = (post) => {
+        setEditPostId(post._id);
+        setFormData({
+            title: post.title,
+            content: post.content,
+            images: post.images,
+            links: post.links,
+            codes: post.codes
+        });
+    };
+
+    // Editar postagem (Salva as alterações)
+    const handleSave = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.put(`http://localhost:5000/api/fundamentals/${editPostId}`, formData);
+            alert("Postagem atualizada!");
+            setEditPostId(null);
+            fetchPosts(page);
+        } catch(error) {
+            console.error("Erro ao atualizar postagem:" + error);
+            alert("Não foi possível atualizar a postagem");
+        }
+    };
+
+    // Editar postagem (Lida com as mudanças nos campos)
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    // Deleta postagem
+    const handleDelete = async (id) => {
+        const confirmDelete = window.confirm("Tem certeza que deseja excluir esta postagem?");
+
+        if (confirmDelete) {
+            try {
+                await axios.delete(`http://localhost:5000/api/fundamentals/${id}`);
+                alert("Postagem excluída com sucesso!");
+                fetchPosts(page);
+            } catch(error) {
+                console.error("Erro ao excluir post: " + error);
+                alert("Não foi possível excluir este post.");
+            }
+        }
+    };
 
     return(
         <div className="fundamentals">
@@ -65,6 +119,66 @@ const Fundamentals = () => {
                     posts && posts.map((post) => (
                         <div key={post._id} className="fundamentals_card">
                             <h3 className="fundamentals_card_title">{post.title}</h3>
+
+                            <div className="fundamentals_card_actions">
+                                <FaEdit
+                                    className="icon edit-icon"
+                                    onClick={() => handleEdit(post)}
+                                    title="Editar esta postagem."
+                                />
+                                <FaTrash
+                                    className="icon delete-icon"
+                                    onClick={() => handleDelete(post._id)}
+                                    title="Apagar esta postagem."
+                                />
+                            </div>
+
+                            {editPostId === post._id && (
+                                <form className="edit_form" onSubmit={handleSave}>
+                                    <input
+                                        type="text"
+                                        name="title"
+                                        value={formData.title}
+                                        onChange={handleChange}
+                                        placeholder="Título"
+                                    />
+
+                                    <textarea
+                                        name="content"
+                                        value={formData.content}
+                                        onChange={handleChange}
+                                        placeholder="Conteúdo"
+                                    />
+
+                                    <input
+                                        type="text"
+                                        name="images"
+                                        value={formData.images}
+                                        onChange={handleChange}
+                                        placeholder="Link da imagem"
+                                    />
+                                    <input
+                                        type="text"
+                                        name="links"
+                                        value={formData.links}
+                                        onChange={handleChange}
+                                        placeholder="Link da fonte"
+                                    />
+
+                                    <textarea
+                                        name="codes"
+                                        value={formData.codes}
+                                        onChange={handleChange}
+                                        placeholder="Código"
+                                    />
+
+                                    <button type="submit">Atualizar</button>
+                                    <button type="button" onClick={() => setEditPostId(null)}>
+                                        Cancelar
+                                    </button>
+                                </form>
+                            )}
+
                             <p className="fundamentals_card_content">{post.content}</p>
                             <p className="fundamentals_card_date">{new Date(post.createdAt).toLocaleDateString()}</p>
                             <img
