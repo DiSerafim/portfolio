0ed0ed66bf45a9568dc431a672a89810cc72ed9f
DiSerafim <diegoserafim1@gmail.com>
2025-02-19 13:25:35 -0300

    criado arquivo para armazenar informações dos projetos desenvolvidos

---
+++ frontend/src/components/dataProjects.js
@@ -0,0 +1,11 @@
+const dataProjects = [
+  {
+    id: 1,
+    name: "Lições Aprendidas",
+    category: "fundamentals",
+    image: "../img/projeto-lessons-learned-mini.png",
+    url: "../../public/projetos/sd-010-b-project-lessons-learned/index.html",
+  },
+];
+
+export default dataProjects;
