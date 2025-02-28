121a266b6ee2daeee40c4bc746e1d8ae1f35bb7c
DiSerafim <diegoserafim1@gmail.com>
2025-02-27 21:09:43 -0300

    Criada a página JavaScript - Funções

--- frontend/public/desenvolvimento-web/fundamentos/fundamentos-pagina13.js
+++ frontend/public/desenvolvimento-web/fundamentos/fundamentos-pagina13.js
@@ -247,3 +247,15 @@ for (let i = 0; i < n1; i += 1) {
   inputLine2 = "";
   inputPosition -= 1;
 }
+
+let matriz = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+for (let i = 0; i < matriz.length; i++) {
+  for (let j = 0; j < matriz[i].length; j++) {
+    console.log(`Elemento [${i}][${j}]: ${matriz[i][j]}`);
+  }
+}
