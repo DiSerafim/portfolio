5573f3cc669c63c87cf61a16a62a2d06df68269a
DiSerafim <diegoserafim1@gmail.com>
2025-07-15 22:55:37 -0300

    alterado a configuração da paginação para exibir 8 páginas na tela

--- frontend/src/components/Pagination.js
+++ frontend/src/components/Pagination.js
@@ -1,33 +1,62 @@
 import React from "react";
-import PropTypes from "prop-types";
+import "./Pagination.css";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const Pagination = ({ page, totalPages, loading, onPageChange }) => {
+  const renderPageNumbers = () => {
+    const totalNumbersToShow = 8;
+    const pages = [];
+
+    let start = Math.max(1, page - Math.floor(totalNumbersToShow / 2));
+    let end = start + totalNumbersToShow - 1;
+
+    if (end > totalPages) {
+      end = totalPages;
+      start = Math.max(1, end - totalNumbersToShow + 1);
+    }
+
+    if (start > 1) {
+      pages.push(1);
+      if (start > 2) pages.push(<FaArrowLeft />);
+    }
+
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+
+    if (end < totalPages) {
+      if (end < totalPages - 1) pages.push(<FaArrowRight /> );
+      pages.push(totalPages);
+    }
+
+    return pages.map((p, index) => (
+      <button
+        key={index}
+        onClick={() => typeof p === "number" && onPageChange(p)}
+        disabled={p === "..."}
+        className={`pagination-btn ${p === page ? "active" : ""}`}
+      >
+        {p}
+      </button>
+    ));
+  };
+
   return (
     <div className="pagination">
-      <button
-        disabled={loading || page === 1}
-        onClick={() => onPageChange("prev")}
-      >
+      <button onClick={() => onPageChange(page - 1)} disabled={page === 1}>
         Anterior
       </button>
-      <span>
-        {page} | {totalPages}
-      </span>
+
+      {renderPageNumbers()}
+
       <button
-        disabled={loading || page === totalPages}
-        onClick={() => onPageChange("next")}
+        onClick={() => onPageChange(page + 1)}
+        disabled={page === totalPages}
       >
         Próxima
       </button>
     </div>
   );
-};
-
-Pagination.propTypes = {
-  page: PropTypes.number.isRequired,
-  totalPages: PropTypes.number.isRequired,
-  loading: PropTypes.bool.isRequired,
-  onPageChange: PropTypes.func.isRequired,
 };
 
 export default Pagination;
